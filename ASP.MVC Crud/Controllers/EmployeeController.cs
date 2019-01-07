74a7349e574a9e48c00d5b5ed6dbe91bcef3020b
Gagulo <grzegorz.sokalski@hotmail.com>
2019-01-07 16:02:51 +0100

    Styling the table

---
+++ ASP.MVC Crud/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using ASP.MVC_Crud.Models;
+
+namespace ASP.MVC_Crud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (ModelDB db = new ModelDB())
+            {
+                List<ASPMVCEmployee> employeeList = db.ASPMVCEmployees.ToList<ASPMVCEmployee>();
+                return Json(new { data = employeeList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View(new ASPMVCEmployee());
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+    }
+}
